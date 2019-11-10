43ae29dd1ad7dac4834e97a29537220e72bb95a6
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-11-10 17:28:05 -0300

    feature list users

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,24 +1,81 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ItemProfileButton,
+  ProfileButtonText,
+} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handlerAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+    this.setState({
+      users: [...users, data],
+    });
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handlerAddUser}
+          />
+          <SubmitButton onPress={this.handlerAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              <ItemProfileButton>
+                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+              </ItemProfileButton>
+            </User>
+          )}
         />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
 Main.navigationOptions = {
   title: 'Usuários',
